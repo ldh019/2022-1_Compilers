2c93508fd2446993defd976b5ace4bd0094ffd87
Hoon <ldh019@naver.com>
2022-04-14 15:16:09 +0900

    4/14 python code done

--- pythonProject/main.py
+++ pythonProject/main.py
@@ -1,7 +1,8 @@
-stringFlag = False
-integerFlag = False
-beforeIntFlag = False
-assignFlag = False
+stringFlag = False  # if this flag is true, letter will include in string
+integerFlag = False  # if this flag is true, letter will include in integer
+beforeIntFlag = False  # if present letter is - and this flag is true, it means
+doneFlag = False  # if next character is last character of this token, it turns true
+wordFlag = False  # if this token includes in id, keyword, type, it turns true
 
 LETTER = []
 DIGIT = []
@@ -9,18 +10,19 @@ ZERO = ['0']
 WHITESPACE = ['\t', '\n', ' ']
 MINUS = ['-']
 OPERATOR = ['+', '*', '/']
-OTHERS = []
+OTHERS = ['(', ')', '{', '}', ',', ';']
 
 
 def tokenize(input):
     ret = []
-    if stringFlag:
+    if stringFlag:  # string token
         ret.append("STRING")
         ret.append(input)
-    elif integerFlag:
+    elif integerFlag:  # integer token
         ret.append("INTEGER")
         ret.append(input)
 
+    # variable type token
     elif input in ["int", "INT"]:
         ret.append("VARTYPE")
         ret.append("INT")
@@ -28,6 +30,7 @@ def tokenize(input):
         ret.append("VARTYPE")
         ret.append("CHAR")
 
+    # keyword token
     elif input in ["if", "IF"]:
         ret.append("KEYWORD")
         ret.append("IF")
@@ -41,6 +44,15 @@ def tokenize(input):
         ret.append("KEYWORD")
         ret.append("RETURN")
 
+    # operator token
+    elif input in OPERATOR + MINUS:
+        ret.append("OP")
+        ret.append(input)
+    elif input == '=':
+        ret.append("ASSIGN")
+        ret.append(input)
+
+    # compare operation token
     elif input == "<":
         ret.append("COMPARE")
         ret.append("<")
@@ -60,6 +72,7 @@ def tokenize(input):
         ret.append("COMPARE")
         ret.append("==")
 
+    # other character token
     elif input == ";":
         ret.append("SEMICOLON")
     elif input == ",":
@@ -73,22 +86,96 @@ def tokenize(input):
     elif input == ")":
         ret.append("RPAREN")
 
+    # identifier token
     else:
         ret.append("ID")
         ret.append(input)
 
-    return ret
+    output.append(ret)
+
 
 for i in range(65, 123):
     LETTER.append(chr(i))
     LETTER.append(chr(i + 32))
-for i in range(49, 59):
+for i in range(49, 58):
     DIGIT.append(chr(i))
 
 output = []
+string = ""
 
 with open("test.c", "rt") as fin:
-    while fin.read() != '':
+    buff1 = fin.read(1)
+    while True:
+        if buff1 == '':
+            break
+        buff2 = fin.read(1)
+        if stringFlag:
+            string += buff1
+        elif doneFlag:
+            string += buff1
+            tokenize(string)
+            doneFlag = False
+        elif buff1 in LETTER:
+            if wordFlag:
+                string += buff1
+            else:
+                string = buff1
+                wordFlag = True
+            if buff2 not in LETTER + ZERO + DIGIT:
+                tokenize(string)
+                string = ""
+                wordFlag = False
+        elif buff1 in ZERO + DIGIT:
+            if integerFlag:
+                string += buff1
+                if buff2 not in ZERO + DIGIT:
+                    tokenize(string)
+                    string = ""
+                    integerFlag = False
+                    beforeIntFlag = True
+            elif wordFlag:
+                string += buff1
+                if buff2 not in ZERO + DIGIT + LETTER:
+                    tokenize(string)
+                    string = ""
+                    wordFlag = False
+            elif buff1 in ZERO:
+                integerFlag = True
+                tokenize(buff1)
+                beforeIntFlag = True
+                integerFlag = False
+            else:
+                string = buff1
+                integerFlag = True
+                if buff2 not in ZERO + DIGIT:
+                    tokenize(string)
+                    integerFlag = False
+        elif buff1 in OPERATOR:
+            tokenize(buff1)
+        elif buff1 in MINUS:
+            if beforeIntFlag:
+                tokenize(buff1)
+                beforeIntFlag = False
+            else:
+                string = buff1
+                integerFlag = True
+        elif buff1 in OTHERS:
+            tokenize(buff1)
+        elif buff1 in ['<', '>', '!', '=']:
+            string = buff1
+            if buff2 == '=':
+                doneFlag = True
+            else:
+                tokenize(string)
+        elif buff1 == '"':
+            if stringFlag:
+                string += buff1
+                tokenize(string)
+                stringFlag = False
+            else:
+                string = buff1
+                stringFlag = True
+        buff1 = buff2
 
 
 with open("test.out", "wt") as fout:
---
+++ pythonProject/test.c
@@ -0,0 +1,8 @@
+int main(int argv, char argc) {
+int a = -1;
+char b = 1-2;
+if(a == 1) return 1;
+else return -2;
+while(1) { a = 1};
+ return 0;
+}
